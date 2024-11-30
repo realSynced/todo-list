0c1185201a06f2cf8242b673bacacd11d25ad53a
Abraham A. <xtremeyolotube10@gmail.com>
2024-11-30 10:59:52 -0500

    Todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,79 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [taskInput, setTaskInput] = useState('');
+  const [firstTime, setFirstTime] = useState(true);
+  const [tasks, setTasks] = useState(['Example 1', 'Example 2', 'Example 3']);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (firstTime && taskInput.trim() !== '') {
+      setTaskInput('');
+      setTasks([]); // clear examples
+      setTasks([...tasks, taskInput.trim()]);
+      setFirstTime(false);
+    } else if (firstTime && taskInput.trim() === '') {
+      alert('Please enter a task before submitting!');
+      return;
+    }
+
+    if(!firstTime && taskInput.trim() !== '') {
+      setTaskInput('');
+      setTasks([...tasks, taskInput.trim()]);
+    } else if(!firstTime && taskInput.trim() === '') {
+      alert('Please enter a task before submitting!');
+      return;
+    }
+  }
+
+
   return (
     <div className="flex justify-center w-full min-h-screen p-5">
       <main className="flex flex-col w-full items-center">
         <div className="">
           <h1 className="text-2xl font-bold ">To-Do List</h1>
         </div>
-        <div className="w-[30rem] bg-[#F3F4F6] rounded-3xl h-max p-5">
-          <div className="bg-white text-gray-500 flex items-center rounded-r-full">
-            <div className="p-2">
-              <h1>Add a new task</h1>
-            </div>
-            <div className="ml-auto">
-              <button className="text-white text-base bg-black p-2 rounded-l-xl transition-all duration-200 ease-in-out
-                hover:width-1/2 hover:bg-gray-800 hover:rounded-3xl hover:shadow-lg
-              ">Add a Task</button>
-            </div>
-          </div>
+        <div className="w-[30rem] bg-lightgray rounded-3xl h-max p-5">
+          <div className="bg-white text-gray-500 rounded-r-full">
+            <form onSubmit={addTask} className="flex items-center">
+              <div className="p-2">
+                <input
+                  type="text"
+                  name="task_input"
+                  placeholder="Add a new task"
+                  className=" focus:outline-lightgray w-72 p-2 rounded-xl"
+                  value={taskInput}
+                  onChange={(e) => setTaskInput(e.target.value)}
 
+                  />
+              </div>
+              <div className="ml-auto">
+                <button type="submit" className="h-full p-3  text-white text-base bg-black rounded-l-xl hover hover:bg-gray-800 hover:rounded-3xl hover:shadow-lg transition-all duration-200 ease-in-out">
+                  Add Task
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="mt-5">
+            <ul className="flex flex-col space-y-4">
+              {tasks.map((task, index) => {
+                return (
+                  <li key={index} className="bg-white text-gray-500 flex items-center rounded-r-full">
+                    <div className="p-2">
+                      <h1>{task}</h1>
+                    </div>
+                    <div className="ml-auto">
+                      <button onClick={() => setTasks(tasks.filter((_, i) => i !== index))} className="text-white text-base bg-black p-2 rounded-l-xl transition-all duration-200 ease-in-out
+                        hover:bg-gray-800
+                      ">Delete</button>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       </main>
 
